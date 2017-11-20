20e072b55130476697dcb2415dd2d63503d315eb
Bcpratt2000 <bcpratt2000@gmail.com>
2017-11-20 13:46:06 -0700

    moved from sftp to rsync

--- bashBackup.sh
+++ bashBackup.sh
@@ -5,50 +5,51 @@
 
 
 #dependencies
+        #rsync
         #lftp
-        #tar
+	#tar
 
 #config variables
 
 
 
-remote_server='sftp://xxx.xxx.xxx.xxx'
-remote_dir='/mnt/bkp_drive'
-remote_port=''
-remote_user=''
-remote_password=''
-local_directory='/mnt/FTP_drive/'
+remote_server='google.bcpratt.com'
+remote_dir='/mnt/drive'
+remote_port='22'
+remote_user='bcpratt2000'
+local_directory='/mnt/FTP_drive'
 
 tmp_directory='/mnt/FTP_drive/tmp/'
 
 
 #program variables
 
-day=$(date +%A)
+day=$(date +%F)
 
 #start program
-mkdir $tmp_directory
+
 cd $tmp_directory
 
-rm *.tar.gz # clear tmp folder
+rm *.tar # clear tmp folder
 
 echo 'Compressing file'
-tar -vczf $day.tar.gz $local_directory
+tar -vcf $day.tar $local_directory
 
 echo 'Changing Permissions'
-chmod 777 $day.tar.gz
+chmod 777 $day.tar
 
 echo 'Starting server transfer'
-lftp -p $remote_port -u $remote_user,$remote_password $remote_server << --EOF--
-        mkdir $remote_dir
-        cd $remote_dir
-        rm $day.tar.gz
-        put $day.tar.gz
-        quit
---EOF--
+
+rsync -zav $day.tar $remote_user@$remote_server:$remote_dir
+#lftp -p $remote_port -u $remote_user $remote_server << --EOF--
+#	cd $remote_dir
+#	rm $day.tar.gz
+#	put $day.tar.gz
+#	quit
+#--EOF--
 
 ls -lh
 echo 'Removing temporary file'
-rm *.tar.gz
+rm *.tar
 
 echo 'All operations completed'
